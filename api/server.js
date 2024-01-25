64be7c837fc351f1b906dba2158c424b409afeb4
Flânelon DJorge Silva Salazar <flanelon18@gmail.com>
2024-01-25 16:53:00 -0300

    add mongoose database connection

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -6,6 +7,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(process.env.PORT, () => {
-  console.log("running on PORT:", process.env.PORT);
-});
+mongoose
+  .connect(process.env.DATA_BASE_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("server is running on PORT:", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
