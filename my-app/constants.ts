eed6cad7a8fe4906c0ef568361b0d3d442249fd3
Flânelon DJorge Silva Salazar <flanelon18@gmail.com>
2024-01-25 17:12:20 -0300

    add contants - sizes and colors

---
+++ my-app/constants.ts
@@ -0,0 +1,21 @@
+export const SIZES = {
+  EXTRA_SMALL: 15,
+  SMALL: 20,
+  MEDIUM: 25,
+  LARGE: 30,
+  EXTRA_LARGE: 35,
+};
+
+export const COLORS = {
+  background_grey: "#ECECEC",
+  light_green: "#D8EAF7",
+  green: "#2D4D52",
+  white: "#fff",
+  gray_background: "#F7F7F7",
+  dark_gray: "#7E8893",
+  red: "#ff0000",
+  purple: "#6C63FF",
+  light_purple: "#3F3D56",
+  success_color: "#2AD265",
+  alert_color: "##F78400",
+};
