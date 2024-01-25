95402202fcbc32a537a6047f69d3e943f4eaf2ea
Flânelon DJorge Silva Salazar <flanelon18@gmail.com>
2024-01-25 17:40:41 -0300

    add stack navigation

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import { RootSiblingParent } from "react-native-root-siblings";
+import BottomNavigationTab from "./navigation/BottomNavigation";
+import Login from "./screens/Auth/Login";
+import Register from "./screens/Auth/Register";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <RootSiblingParent>
+      <NavigationContainer>
+        <StatusBar style="auto" />
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+          initialRouteName="Register"
+        >
+          <Stack.Screen
+            name="BottomNavigationTab"
+            component={BottomNavigationTab}
+          />
+          <Stack.Screen name="Register" component={Register} />
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </RootSiblingParent>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
