d9bd1260197634a0b9b370a88fb821d1f96c1121
Flânelon DJorge Silva Salazar <flanelon18@gmail.com>
2024-01-25 17:01:41 -0300

    Add register router

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const cors = require("cors");
+const User = require("./models/User");
 require("dotenv").config();
 
 const app = express();
@@ -12,6 +14,75 @@ app.get("/", (req, resp) => {
   resp.status(200).json({
     msg: "connected",
   });
+});
+
+// Register Route
+app.post("/auth/register", async (req, resp) => {
+  const { name, email, password, confirmPassword } = req.body;
+  //validate body
+  if (!name) {
+    return resp.status(400).json({
+      code: 400,
+      msg: "nome é obrigatório",
+    });
+  }
+  if (!email) {
+    return resp.status(400).json({
+      code: 400,
+      msg: "email é obrigatório",
+    });
+  }
+  if (!password) {
+    return resp.status(400).json({
+      code: 400,
+      msg: "senha é obrigatório",
+    });
+  }
+  if (!confirmPassword) {
+    return resp.status(400).json({
+      code: 400,
+      msg: "confirmar senha é obrigatório",
+    });
+  }
+
+  if (password !== confirmPassword) {
+    return resp.status(400).json({
+      code: 400,
+      msg: "senhas devem ser iguais",
+    });
+  }
+
+  const user = await User.findOne({
+    email,
+  });
+
+  if (user) {
+    return resp.status(400).json({
+      code: 400,
+      msg: "email já está cadastrado",
+    });
+  }
+
+  try {
+    const hash = await bcrypt.genSalt(12);
+    const hashedPassword = await bcrypt.hash(password, hash);
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    return resp.status(200).json({
+      code: 200,
+      msg: "cadastrado com sucesso!",
+      data: {
+        name: newUser.name,
+        email: newUser.email,
+      },
+    });
+  } catch (error) {
+    return resp.status(400).json(error);
+  }
 });
 
 mongoose
